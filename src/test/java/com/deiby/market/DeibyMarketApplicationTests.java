b87c70e00c62908135782d4cacfa91049e1c2d5b
deibyalejo99 <deibyalejo123@gmail.com>
2022-05-18 19:00:33 -0500

    first commit

---
+++ src/test/java/com/deiby/market/DeibyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deiby.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeibyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
