d997b9d9af42cde285e9a1317f1a8082beca4886
deibyalejo99 <deibyalejo123@gmail.com>
2022-06-05 12:21:23 -0500

    FIler jwt

--- src/main/java/com/deiby/market/web/security/SecurityConfig.java
+++ src/main/java/com/deiby/market/web/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.deiby.market.web.security;
 
 import com.deiby.market.domain.service.UserDetailsService;
+import com.deiby.market.web.security.filter.JwFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,12 +9,17 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwFilterRequest jwFilterRequest;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -23,7 +29,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeHttpRequests().antMatchers("/**/authenticate").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwFilterRequest, UsernamePasswordAuthenticationFilter.class);
+
     }
 
     @Override
